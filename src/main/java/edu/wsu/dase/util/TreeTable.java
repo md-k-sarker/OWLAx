26071e140d02c1fb22b725ec350d6ad92ddf81b5
Md Kamruzzaman Sarker <mdkamruzzamansarker@gmail.com>
2016-06-18 20:32:04 -0400

    implementing treetable

---
+++ src/main/java/edu/wsu/dase/util/TreeTable.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package edu.wsu.dase.util;
+
+/**
+ * @author sarker
+ *
+ */
+public class TreeTable {
+
+	/**
+	 * 
+	 */
+	public TreeTable() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
